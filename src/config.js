8c3c413fdb2f82444b5bfbb3198f927653a4b9b2
Nguyên Hồ <nguyen.ho@gosmartlog.com>
2024-12-21 20:33:40 +0700

    Update

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 const config = {
     environment: import.meta.env.VITE_ENVIRONMENT,
-    urlFrontend: import.meta.env.VITE_URL_FRONTEND,
     urlBackend: import.meta.env.VITE_URL_BACKEND,
     prefix: import.meta.env.VITE_PREFIX,
     unsplash_access_key: import.meta.env.VITE_UNSPLASH_ACCESS_KEY,
